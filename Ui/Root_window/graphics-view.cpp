778d72a9a91828c048ba8e262429d730ef5f29d1
Marcin <thewitcher87@gmail.com>
2012-04-26 21:26:49 +0200

    [Bug] Fix bug with bad configured checkpoint. Now vehicle stops on red light.

--- Ui/Root_window/graphics-view.cpp
+++ Ui/Root_window/graphics-view.cpp
@@ -11,10 +11,10 @@
 
 
 int GraphicsView::S_NEW_CAR_FREQUENCY = 5000;
-int GraphicsView::S_CAR_COUNT = 5;
+int GraphicsView::S_CAR_COUNT = 10;
 int GraphicsView::S_CAR_SPEED = 1;
 int GraphicsView::S_BUS_SPEED = 2;
-int GraphicsView::S_BUS_COUNT = 4;
+int GraphicsView::S_BUS_COUNT = 10;
 
 /*!
  * GraphicsView is a subclass of QGraphicsView. It was created for convenience. In constructor there are already four method, which
